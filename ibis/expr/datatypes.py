4959855ae8cdb91db9fa125a20fd6307c3ff99c7
Krisztián Szűcs <szucs.krisztian@gmail.com>
2018-02-22 03:52:11 +0100

    wip

--- ibis/expr/datatypes.py
+++ ibis/expr/datatypes.py
@@ -1122,6 +1122,8 @@ def can_cast_subtype(source, target, **kwargs):
 
 @castable.register(Any, DataType)
 @castable.register(DataType, Any)
+@castable.register(Any, Any)
+@castable.register(Null, Any)
 @castable.register(Integer, Category)
 @castable.register(Integer, (Integer, Floating, Decimal))
 @castable.register(Floating, Decimal)
