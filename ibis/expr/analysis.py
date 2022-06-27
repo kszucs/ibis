d29c791bb103469325497979b38d50d0e8c82e01
Krisztián Szűcs <szucs.krisztian@gmail.com>
2022-06-28 00:13:08 +0200

    disable linage tests since they are not used; don't block on self references when locating projections

--- ibis/expr/analysis.py
+++ ibis/expr/analysis.py
@@ -618,10 +618,18 @@ def find_root_tables(node):
 
 
 def _find_projections(node):
+    assert isinstance(node, ops.Node), type(node)
+
     if isinstance(node, ops.Selection):
         # remove predicates and sort_keys, so that child tables are considered
         # equivalent even if their predicates and sort_keys are not
+
+        # FIXME(kszucs): may not need to restrict the selection
+        # return lin.proceed, node
         return lin.proceed, node._projection
+
+    elif isinstance(node, ops.SelfReference):
+        return lin.proceed, node
     elif node.blocks():
         return lin.halt, node
     else:
@@ -635,6 +643,7 @@ def shares_all_roots(exprs, parents):
     # unique table dependencies of exprs and parents
     exprs_deps = set(lin.traverse(_find_projections, exprs))
     parents_deps = set(lin.traverse(_find_projections, parents))
+
     return exprs_deps <= parents_deps
 
 
@@ -678,7 +687,7 @@ def flatten_predicate(node):
       b string
     r0.b == 'foo'
     """
-    assert isinstance(node, (list, ops.Node)), type(node)
+    assert all(isinstance(arg, ops.Node) for arg in util.promote_list(node))
 
     def predicate(node):
         if isinstance(node, ops.And):
@@ -690,6 +699,8 @@ def flatten_predicate(node):
 
 
 def is_analytic(node):
+    assert all(isinstance(arg, ops.Node) for arg in util.promote_list(node))
+
     def predicate(node):
         if isinstance(node, (ops.Reduction, ops.Analytic)):
             return lin.halt, True
@@ -727,7 +738,7 @@ def is_reduction(node):
     -------
     check output : bool
     """
-    assert isinstance(node, ops.Node), type(node)
+    assert all(isinstance(arg, ops.Node) for arg in util.promote_list(node))
 
     def predicate(node):
         if isinstance(node, ops.Reduction):
@@ -772,8 +783,7 @@ def find_predicates(node, flatten=True):
 
 
 def find_subqueries(node: ops.Node) -> Counter:
-    for n in util.promote_list(node):
-        assert isinstance(n, ops.Node), type(n)
+    assert all(isinstance(arg, ops.Node) for arg in util.promote_list(node))
 
     counts = Counter()
 
