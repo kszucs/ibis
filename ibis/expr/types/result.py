767145324be5f8fe45ce712b2d99ce69871586f0
Krisztián Szűcs <szucs.krisztian@gmail.com>
2022-09-07 17:38:25 +0200

    feat(ir): seamlessly convert between different result types

--- ibis/expr/types/result.py
+++ ibis/expr/types/result.py
@@ -14,4 +14,22 @@ class ColumnResult(Result):
 
 
 class TableResult(Result):
-    pass
+    def to_csv(self, path):
+        table = self.op().table.to_expr()
+        return table.to_csv(path)
+
+    def to_parquet(self, path):
+        table = self.op().table.to_expr()
+        return table.to_parquet(path)
+
+    def to_pylist(self):
+        table = self.op().table.to_expr()
+        return table.to_pylist()
+
+    def to_pandas(self):
+        table = self.op().table.to_expr()
+        return table.to_pandas()
+
+    def to_pyarrow(self):
+        table = self.op().table.to_expr()
+        return table.to_pyarrow()
