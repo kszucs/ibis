b3de2cf8d4f6ed61bd69d7c758889857c0595de8
Krisztián Szűcs <szucs.krisztian@gmail.com>
2023-11-19 18:19:07 +0100

    integration step 8 - add support for having when constructing an aggregation

--- ibis/expr/api.py
+++ ibis/expr/api.py
@@ -450,7 +450,7 @@ def _memtable_from_pyarrow_table(
     schema: SupportsSchema | None = None,
     columns: Iterable[str] | None = None,
 ):
-    from ibis.expr.operations.relations import PyArrowTableProxy
+    from ibis.expr.operations.newrels import PyArrowTableProxy
 
     if columns is not None:
         assert schema is None, "if `columns` is not `None` then `schema` must be `None`"
@@ -472,7 +472,7 @@ def _memtable_from_dataframe(
 ) -> Table:
     import pandas as pd
 
-    from ibis.expr.operations.relations import PandasDataFrameProxy
+    from ibis.expr.operations.newrels import PandasDataFrameProxy
 
     df = pd.DataFrame(data, columns=columns)
     if df.columns.inferred_type != "string":
