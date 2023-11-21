527f01da7a20fe65ba620a0bd8b85c11e68e0378
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-11-21 13:25:45 -0500

    chore: fix foreign field compilation

--- ibis/backends/tests/tpch/snapshots/test_h15/test_tpc_h15/duckdb/h15.sql
+++ ibis/backends/tests/tpch/snapshots/test_h15/test_tpc_h15/duckdb/h15.sql
@@ -1,9 +1,9 @@
 SELECT
-  t6.s_suppkey,
-  t6.s_name,
-  t6.s_address,
-  t6.s_phone,
-  t6.total_revenue
+  t8.s_suppkey AS s_suppkey,
+  t8.s_name AS s_name,
+  t8.s_address AS s_address,
+  t8.s_phone AS s_phone,
+  t8.total_revenue AS total_revenue
 FROM (
   SELECT
     *
@@ -12,58 +12,90 @@ FROM (
       *
     FROM (
       SELECT
-        *
+        t0.s_suppkey AS s_suppkey,
+        t0.s_name AS s_name,
+        t0.s_address AS s_address,
+        t0.s_nationkey AS s_nationkey,
+        t0.s_phone AS s_phone,
+        t0.s_acctbal AS s_acctbal,
+        t0.s_comment AS s_comment,
+        t3.l_suppkey AS l_suppkey,
+        t3.total_revenue AS total_revenue
       FROM "supplier" AS t0
       INNER JOIN (
         SELECT
-          t1.l_suppkey,
+          t2.l_suppkey AS l_suppkey,
           SUM((
-            t1.l_extendedprice * (
-              CAST(1 AS TINYINT) - t1.l_discount
+            t2.l_extendedprice * (
+              CAST(1 AS TINYINT) - t2.l_discount
             )
           )) AS total_revenue
-        FROM "lineitem" AS t1
-        WHERE
-          (
-            t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
-          )
-          AND (
-            t1.l_shipdate < MAKE_DATE(1996, 4, 1)
-          )
+        FROM (
+          SELECT
+            *
+          FROM "lineitem" AS t1
+          WHERE
+            (
+              t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
+            )
+            AND (
+              t1.l_shipdate < MAKE_DATE(1996, 4, 1)
+            )
+        ) AS t2
         GROUP BY
           1
-      ) AS t2
+      ) AS t3
         ON (
-          t0.s_suppkey = t2.l_suppkey
+          t0.s_suppkey = t3.l_suppkey
         )
-    ) AS t3
+    ) AS t5
     WHERE
       (
-        t3.total_revenue = (
+        t5.total_revenue = (
           SELECT
-            MAX(t2.total_revenue) AS "Max(total_revenue)"
+            MAX(t5.total_revenue) AS "Max(total_revenue)"
           FROM (
             SELECT
-              t1.l_suppkey,
-              SUM((
-                t1.l_extendedprice * (
-                  CAST(1 AS TINYINT) - t1.l_discount
-                )
-              )) AS total_revenue
-            FROM "lineitem" AS t1
-            WHERE
-              (
-                t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
+              t0.s_suppkey AS s_suppkey,
+              t0.s_name AS s_name,
+              t0.s_address AS s_address,
+              t0.s_nationkey AS s_nationkey,
+              t0.s_phone AS s_phone,
+              t0.s_acctbal AS s_acctbal,
+              t0.s_comment AS s_comment,
+              t3.l_suppkey AS l_suppkey,
+              t3.total_revenue AS total_revenue
+            FROM "supplier" AS t0
+            INNER JOIN (
+              SELECT
+                t2.l_suppkey AS l_suppkey,
+                SUM((
+                  t2.l_extendedprice * (
+                    CAST(1 AS TINYINT) - t2.l_discount
+                  )
+                )) AS total_revenue
+              FROM (
+                SELECT
+                  *
+                FROM "lineitem" AS t1
+                WHERE
+                  (
+                    t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
+                  )
+                  AND (
+                    t1.l_shipdate < MAKE_DATE(1996, 4, 1)
+                  )
+              ) AS t2
+              GROUP BY
+                1
+            ) AS t3
+              ON (
+                t0.s_suppkey = t3.l_suppkey
               )
-              AND (
-                t1.l_shipdate < MAKE_DATE(1996, 4, 1)
-              )
-            GROUP BY
-              1
-          ) AS t2
+          ) AS t5
         )
       )
-  ) AS t5
+  ) AS t7
   ORDER BY
-    t5.s_suppkey ASC
-) AS t6
+    t7.s_suppkey ASC
+) AS t8
