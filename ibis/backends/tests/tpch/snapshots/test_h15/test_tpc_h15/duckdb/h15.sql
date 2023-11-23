97ab7ed55648f1bc902234d960d4525a37a1dbfa
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-11-23 11:29:13 -0500

    chore: regen sql

--- ibis/backends/tests/tpch/snapshots/test_h15/test_tpc_h15/duckdb/h15.sql
+++ ibis/backends/tests/tpch/snapshots/test_h15/test_tpc_h15/duckdb/h15.sql
@@ -53,50 +53,46 @@ FROM (
       (
         t5.total_revenue = (
           SELECT
-            t6."Max(total_revenue)"
+            MAX(t5.total_revenue) AS "Max(total_revenue)"
           FROM (
             SELECT
-              MAX(t5.total_revenue) AS "Max(total_revenue)"
-            FROM (
+              t0.s_suppkey AS s_suppkey,
+              t0.s_name AS s_name,
+              t0.s_address AS s_address,
+              t0.s_nationkey AS s_nationkey,
+              t0.s_phone AS s_phone,
+              t0.s_acctbal AS s_acctbal,
+              t0.s_comment AS s_comment,
+              t3.l_suppkey AS l_suppkey,
+              t3.total_revenue AS total_revenue
+            FROM "supplier" AS t0
+            INNER JOIN (
               SELECT
-                t0.s_suppkey AS s_suppkey,
-                t0.s_name AS s_name,
-                t0.s_address AS s_address,
-                t0.s_nationkey AS s_nationkey,
-                t0.s_phone AS s_phone,
-                t0.s_acctbal AS s_acctbal,
-                t0.s_comment AS s_comment,
-                t3.l_suppkey AS l_suppkey,
-                t3.total_revenue AS total_revenue
-              FROM "supplier" AS t0
-              INNER JOIN (
+                t2.l_suppkey AS l_suppkey,
+                SUM((
+                  t2.l_extendedprice * (
+                    CAST(1 AS TINYINT) - t2.l_discount
+                  )
+                )) AS total_revenue
+              FROM (
                 SELECT
-                  t2.l_suppkey AS l_suppkey,
-                  SUM((
-                    t2.l_extendedprice * (
-                      CAST(1 AS TINYINT) - t2.l_discount
-                    )
-                  )) AS total_revenue
-                FROM (
-                  SELECT
-                    *
-                  FROM "lineitem" AS t1
-                  WHERE
-                    (
-                      t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
-                    )
-                    AND (
-                      t1.l_shipdate < MAKE_DATE(1996, 4, 1)
-                    )
-                ) AS t2
-                GROUP BY
-                  1
-              ) AS t3
-                ON (
-                  t0.s_suppkey = t3.l_suppkey
-                )
-            ) AS t5
-          ) AS t6
+                  *
+                FROM "lineitem" AS t1
+                WHERE
+                  (
+                    t1.l_shipdate >= MAKE_DATE(1996, 1, 1)
+                  )
+                  AND (
+                    t1.l_shipdate < MAKE_DATE(1996, 4, 1)
+                  )
+              ) AS t2
+              GROUP BY
+                1
+            ) AS t3
+              ON (
+                t0.s_suppkey = t3.l_suppkey
+              )
+          ) AS t5
         )
       )
   ) AS t7
--- ibis/backends/tests/tpch/snapshots/test_h20/test_tpc_h20/duckdb/h20.sql
+++ ibis/backends/tests/tpch/snapshots/test_h20/test_tpc_h20/duckdb/h20.sql
@@ -2,82 +2,82 @@ SELECT
   *
 FROM (
   SELECT
-    t3.s_name,
-    t3.s_address
+    t13.s_name AS s_name,
+    t13.s_address AS s_address
   FROM (
     SELECT
       *
     FROM (
       SELECT
-        *
+        t0.s_suppkey AS s_suppkey,
+        t0.s_name AS s_name,
+        t0.s_address AS s_address,
+        t0.s_nationkey AS s_nationkey,
+        t0.s_phone AS s_phone,
+        t0.s_acctbal AS s_acctbal,
+        t0.s_comment AS s_comment,
+        t1.n_nationkey AS n_nationkey,
+        t1.n_name AS n_name,
+        t1.n_regionkey AS n_regionkey,
+        t1.n_comment AS n_comment
       FROM "supplier" AS t0
       INNER JOIN "nation" AS t1
         ON (
-          t0.s_nationkey
-        ) = (
-          t1.n_nationkey
+          t0.s_nationkey = t1.n_nationkey
         )
-    ) AS t2
+    ) AS t8
     WHERE
       (
-        t2.n_name
-      ) = (
-        'CANADA'
+        t8.n_name = 'CANADA'
       )
-      AND t2.s_suppkey IN (
+      AND t8.s_suppkey IN ((
         SELECT
-          t1.ps_suppkey
+          t11.ps_suppkey AS ps_suppkey
         FROM (
           SELECT
             *
-          FROM "partsupp" AS t0
+          FROM "partsupp" AS t2
           WHERE
-            t0.ps_partkey IN (
+            t2.ps_partkey IN ((
               SELECT
-                t1.p_partkey
+                t6.p_partkey AS p_partkey
               FROM (
                 SELECT
                   *
-                FROM "part" AS t0
+                FROM "part" AS t3
                 WHERE
-                  t0.p_name LIKE 'forest%'
-              ) AS t1
-            )
+                  t3.p_name LIKE 'forest%'
+              ) AS t6
+            ))
             AND (
-              t0.ps_availqty
-            ) > (
-              (
-                SELECT
-                  SUM(t0.l_quantity) AS "Sum(l_quantity)"
-                FROM "lineitem" AS t0
-                WHERE
-                  (
-                    t0.l_partkey
-                  ) = (
-                    ps_partkey
-                  )
-                  AND (
-                    t0.l_suppkey
-                  ) = (
-                    ps_suppkey
-                  )
-                  AND (
-                    t0.l_shipdate
-                  ) >= (
-                    MAKE_DATE(1994, 1, 1)
-                  )
-                  AND (
-                    t0.l_shipdate
-                  ) < (
-                    MAKE_DATE(1995, 1, 1)
-                  )
-              ) * (
-                CAST(0.5 AS DOUBLE)
+              t2.ps_availqty > (
+                (
+                  SELECT
+                    SUM(t7.l_quantity) AS "Sum(l_quantity)"
+                  FROM (
+                    SELECT
+                      *
+                    FROM "lineitem" AS t4
+                    WHERE
+                      (
+                        t4.l_partkey = t2.ps_partkey
+                      )
+                      AND (
+                        t4.l_suppkey = t2.ps_suppkey
+                      )
+                      AND (
+                        t4.l_shipdate >= MAKE_DATE(1994, 1, 1)
+                      )
+                      AND (
+                        t4.l_shipdate < MAKE_DATE(1995, 1, 1)
+                      )
+                  ) AS t7
+                ) * CAST(0.5 AS DOUBLE)
               )
             )
-        ) AS t1
-      )
-  ) AS t3
-) AS t4
+        ) AS t11
+      ))
+  ) AS t13
+) AS t14
 ORDER BY
-  t4.s_name ASC
+  t14.s_name ASC
