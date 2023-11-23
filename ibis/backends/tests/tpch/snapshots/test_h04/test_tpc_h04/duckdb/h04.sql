c521e449595aeefe22b5f427bb59031b50402fb3
Phillip Cloud <417981+cpcloud@users.noreply.github.com>
2023-11-23 11:34:39 -0500

    chore: fix broken h01 and h04 queries

--- ibis/backends/tests/tpch/snapshots/test_h04/test_tpc_h04/duckdb/h04.sql
+++ ibis/backends/tests/tpch/snapshots/test_h04/test_tpc_h04/duckdb/h04.sql
@@ -1,28 +1,42 @@
 SELECT
-  t0.o_orderpriority,
-  COUNT(*) AS order_count
-FROM "orders" AS t0
-WHERE
-  EXISTS(
-    (
-      SELECT
-        1
-      FROM "lineitem" AS t1
-      WHERE
+  *
+FROM (
+  SELECT
+    t4.o_orderpriority AS o_orderpriority,
+    COUNT(*) AS order_count
+  FROM (
+    SELECT
+      *
+    FROM "orders" AS t0
+    WHERE
+      EXISTS(
         (
-          t1.l_orderkey = t0.o_orderkey
-        ) AND (
-          t1.l_commitdate < t1.l_receiptdate
+          SELECT
+            CAST(1 AS TINYINT) AS "1"
+          FROM (
+            SELECT
+              *
+            FROM "lineitem" AS t1
+            WHERE
+              (
+                (
+                  t1.l_orderkey = t0.o_orderkey
+                ) AND (
+                  t1.l_commitdate < t1.l_receiptdate
+                )
+              )
+          ) AS t2
         )
-    )
-  )
-  AND (
-    t0.o_orderdate >= MAKE_DATE(1993, 7, 1)
-  )
-  AND (
-    t0.o_orderdate < MAKE_DATE(1993, 10, 1)
-  )
-GROUP BY
-  1
+      )
+      AND (
+        t0.o_orderdate >= MAKE_DATE(1993, 7, 1)
+      )
+      AND (
+        t0.o_orderdate < MAKE_DATE(1993, 10, 1)
+      )
+  ) AS t4
+  GROUP BY
+    1
+) AS t5
 ORDER BY
-  t0.o_orderpriority ASC
+  t5.o_orderpriority ASC
