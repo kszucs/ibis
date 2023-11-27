7176269c270a243b58500287fa7b1c2eb7212ad9
Krisztián Szűcs <szucs.krisztian@gmail.com>
2023-11-27 18:47:05 +0100

    refactor(ir): introduce `Value.relations` attribute to accumulate the relations a value depend on

--- ibis/expr/operations/core.py
+++ ibis/expr/operations/core.py
@@ -10,6 +10,7 @@ import ibis.expr.datashape as ds
 import ibis.expr.datatypes as dt
 import ibis.expr.rules as rlz
 from ibis import util
+from ibis.common.annotations import attribute
 from ibis.common.bases import Abstract
 from ibis.common.graph import Node as Traversable
 from ibis.common.grounds import Concrete
@@ -37,6 +38,11 @@ class Node(Concrete, Traversable):
 
     # Avoid custom repr for performance reasons
     __repr__ = object.__repr__
+
+    # TODO(kszucs): hidrate the __children__ traversable attribute
+    # @attribute
+    # def __children__(self):
+    #     return super().__children__
 
 
 # TODO(kszucs): remove this mixin
@@ -119,6 +125,12 @@ class Value(Node, Named, Coercible, DefaultTypeVars, Generic[T, S]):
         ds.Shape
         """
 
+    @attribute
+    def relations(self):
+        """Set of relations the value node depends on."""
+        children = (n.relations for n in self.__children__ if isinstance(n, Value))
+        return frozenset().union(*children)
+
     @property
     @util.deprecated(as_of="7.0", instead="use .dtype property instead")
     def output_dtype(self):
@@ -160,9 +172,13 @@ class Unary(Value):
 
     arg: Value
 
-    @property
+    @attribute
     def shape(self) -> ds.DataShape:
         return self.arg.shape
+
+    @attribute
+    def relations(self):
+        return self.arg.relations
 
 
 @public
@@ -172,9 +188,13 @@ class Binary(Value):
     left: Value
     right: Value
 
-    @property
+    @attribute
     def shape(self) -> ds.DataShape:
         return max(self.left.shape, self.right.shape)
+
+    @attribute
+    def relations(self):
+        return self.left.relations | self.right.relations
 
 
 @public
